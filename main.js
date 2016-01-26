0933b11a0648ff624533e97cbe9219733d2d5b1f
Konstantinos Psimoulis <kosta@noima.com>
2016-01-25 20:49:43 -0500

    Adding code to skip ONLINE courses

--- main.js
+++ main.js
@@ -11,40 +11,42 @@ if (window.jQuery) {
 
         for (i = 0; i < totalCourses; i++) {
             tmp = $("#DERIVED_SSS_SCL_SSR_MTG_SCHED_LONG\\$" + i).text().split("\n")[0].split("-")[0].trim();
-            byday = tmp.split(" ")[0].match(/.{1,2}/g);
-            $.each(byday, function (index, item) {
-                byday[index] = item.toUpperCase();
-            });
-            startTime = tmp.split(" ")[1].match(/(\d+)(?::(\d\d))?\s*(P?)/);
-            endTime = $("#DERIVED_SSS_SCL_SSR_MTG_SCHED_LONG\\$" + i).text().split("\n")[0].split("-")[1].trim().match(/(\d+)(?::(\d\d))?\s*(P?)/);
-            begin = new Date();
-            begin.setMonth(startMonth);
-            begin.setDate(startDay);
-            begin.setHours(parseInt(startTime[1]) + (startTime[3] ? 12 : 0));
-            begin.setMinutes(parseInt(startTime[2]) || 0);
-            end = new Date();
-            end.setMonth(startMonth);
-            end.setDate(startDay);
-            end.setHours(parseInt(endTime[1]) + (endTime[3] ? 12 : 0));
-            end.setMinutes(parseInt(endTime[2]) || 0);
-            subject = $("#CLASS_NAME\\$span\\$" + i).text().split("\n")[0];
-            description = $("#CLASS_NAME\\$span\\$" + i).text().split("\n")[1];
-            room = $("#DERIVED_SSS_SCL_SSR_MTG_SCHED_LONG\\$" + i).text().split("\n")[1];
-            rrule = {
-                freq: "WEEKLY",
-                interval: 1,
-                byday: byday,
-                until: endDate
-            }
+            if (tmp.split(" ")[0] != "ONLINE") {
+                byday = tmp.split(" ")[0].match(/.{1,2}/g);
+                $.each(byday, function (index, item) {
+                    byday[index] = item.toUpperCase();
+                });
+                startTime = tmp.split(" ")[1].match(/(\d+)(?::(\d\d))?\s*(P?)/);
+                endTime = $("#DERIVED_SSS_SCL_SSR_MTG_SCHED_LONG\\$" + i).text().split("\n")[0].split("-")[1].trim().match(/(\d+)(?::(\d\d))?\s*(P?)/);
+                begin = new Date();
+                begin.setMonth(startMonth);
+                begin.setDate(startDay);
+                begin.setHours(parseInt(startTime[1]) + (startTime[3] ? 12 : 0));
+                begin.setMinutes(parseInt(startTime[2]) || 0);
+                end = new Date();
+                end.setMonth(startMonth);
+                end.setDate(startDay);
+                end.setHours(parseInt(endTime[1]) + (endTime[3] ? 12 : 0));
+                end.setMinutes(parseInt(endTime[2]) || 0);
+                subject = $("#CLASS_NAME\\$span\\$" + i).text().split("\n")[0];
+                description = $("#CLASS_NAME\\$span\\$" + i).text().split("\n")[1];
+                room = $("#DERIVED_SSS_SCL_SSR_MTG_SCHED_LONG\\$" + i).text().split("\n")[1];
+                rrule = {
+                    freq: "WEEKLY",
+                    interval: 1,
+                    byday: byday,
+                    until: endDate
+                }
 
-            data.push({
-                subject: subject,
-                description: description,
-                rrule: rrule,
-                begin: begin,
-                end: end,
-                location: room
-            })
+                data.push({
+                    subject: subject,
+                    description: description,
+                    rrule: rrule,
+                    begin: begin,
+                    end: end,
+                    location: room
+                })
+            }
         }
 
         var cal = ics();
